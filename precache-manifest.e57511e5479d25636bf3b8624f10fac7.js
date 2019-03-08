26c50411bea8906d4f75c8d69ec5b9b0836813b6
sara abdelaziz <saramohamedaziz@gmail.com>
2019-03-09 01:22:47 +0200

    Updates

---
+++ precache-manifest.e57511e5479d25636bf3b8624f10fac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "522e23537fdd69fde860",
+    "url": "/assignment/static/js/runtime~main.405b42a8.js"
+  },
+  {
+    "revision": "a376f21120b98497dab3",
+    "url": "/assignment/static/js/main.233c8bad.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/assignment/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "a376f21120b98497dab3",
+    "url": "/assignment/static/css/main.89fce634.chunk.css"
+  },
+  {
+    "revision": "1f20c39de66a8d911d970b40912c6ca8",
+    "url": "/assignment/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/assignment/precache-manifest.7b5f274e93150b7c11289472b60e6536.js"
+  "/assignment/precache-manifest.e57511e5479d25636bf3b8624f10fac7.js"
 );
 
 workbox.clientsClaim();
